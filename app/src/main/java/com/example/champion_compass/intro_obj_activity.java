8af554ece86cb0fbce9861a9e838f78cea99d2f9
Vgangaswamy <vgangaswamy@hawk.iit.edu>
2024-04-24 01:17:01 -0500

    app

---
+++ app/src/main/java/com/example/champion_compass/intro_obj_activity.java
@@ -0,0 +1,13 @@
+package com.example.champion_compass;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class intro_obj_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout6);
+    }
+}
+gi
--- app/src/main/java/com/example/champion_compass/welcome_activity.java
+++ app/src/main/java/com/example/champion_compass/welcome_activity.java
@@ -1,6 +1,10 @@
 package com.example.champion_compass;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class welcome_activity extends AppCompatActivity {
@@ -8,5 +12,14 @@ public class welcome_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout5);
+
+        Button button5 = findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(welcome_activity.this, intro_obj_activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
